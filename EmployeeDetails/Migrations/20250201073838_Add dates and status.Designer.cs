9d7c68f15d213e2a7fdbfc91fdb285d40f25ba0b
Mudit Saxena <mdtsxn66@gmail.com>
2025-02-01 13:11:38 +0530

    Define Repositories and added migration

--- EmployeeDetails/Migrations/20250201073838_Add dates and status.Designer.cs
+++ EmployeeDetails/Migrations/20250201073838_Add dates and status.Designer.cs
@@ -4,6 +4,7 @@ using EmployeeDetails.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeDetails.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250201073838_Add dates and status")]
+    partial class Adddatesandstatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmployeeDetails/Migrations/20250201073838_Add dates and status.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeDetails.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adddatesandstatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "tblEmployee",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "tblEmployee",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "tblEmployee",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "tblDepartment",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "tblDepartment",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "tblDepartment",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "tblEmployee");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdateDate",
+                table: "tblEmployee");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "tblEmployee");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "tblDepartment");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdateDate",
+                table: "tblDepartment");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "tblDepartment");
+        }
+    }
+}
